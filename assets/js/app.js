a449e9be4307407ce4469e72186df098e8799ade
YevhenChudnyi <evgeniychudniy@gmail.com>
2018-06-03 21:08:48 +0300

    Created ProjectControllers and templates

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+require('../css/app.css');
+
+// loads the jquery package from node_modules
+var $ = require('jquery');
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,24 +3,53 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Redmine;
 
 class ProjectController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="project_list")
      */
-    public function number()
+    public function list()
     {
-        $number = mt_rand(0, 100);
-
         $client = new Redmine\Client('https://redmine.ekreative.com', '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c');
 
-        dump($client->project->all());
-
         return $this->render('project/list.html.twig', array(
-            'number' => $number,
+            'project_array' =>  $client->project->all()['projects'],
         ));
+    }
+
+    /**
+     * @Route("/project/{projectId}", name="project_show")
+     *
+     * @param integer $projectId
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($projectId = null)
+    {
+        $client = new Redmine\Client('https://redmine.ekreative.com', '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c');
+
+        if ($projectId) {
+            $projectData = $client->project->show($projectId);
+            $issuesData = $client->issue->all(['project_id' => $projectId]);
+
+
+            dump( $projectData['project'] );
+            dump(  $issuesData);
+
+            return $this->render('project/show.html.twig', array(
+                'project' => $projectData ? $projectData['project'] : false,
+                'issues_list' =>  isset($issuesData['issues'])? $issuesData['issues'] : null,
+            ));
+        } else {
+            return $this->redirect($this->generateUrl('project_list', array(
+                'error' =>true,
+                'error_text' => "Project wasn't founded on the server",
+            )), 301
+            );
+        }
     }
 }
